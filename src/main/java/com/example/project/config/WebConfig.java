249d92302bba5fbd32ef3e2cd90214670364189a
hayu6 <hayu6944@gmail.com>
2024-11-02 20:04:11 +0900

    qq

--- src/main/java/com/example/project/config/WebConfig.java
+++ src/main/java/com/example/project/config/WebConfig.java
@@ -1,16 +1,25 @@
 package com.example.project.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration // spring의 설정파일이라는 의미
+@Configuration // Spring의 설정 파일이라는 의미
 public class WebConfig implements WebMvcConfigurer {
-    private String resourcePath = "/upload/**"; // view 에서 접근할 경로
+
+    private String resourcePath = "/upload/**"; // view에서 접근할 경로
     private String savePath = "file:///C:/springboot_img/"; // 실제 파일 저장 경로
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(resourcePath)
                 .addResourceLocations(savePath);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("https://taskmanager-main-ekh8bbgpa3dcbvht.koreacentral-01.azurewebsites.net"); // Azure 배포 도메인 허용
     }
 }
